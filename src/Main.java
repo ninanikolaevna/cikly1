7fc53b298949cccf972e37ca5ca42cd69cc6bb98
ninanikolaevna <ninanikolaevna.t@gmail.com>
2024-09-07 13:05:57 +0300

    Initial after 1 task

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        for (int i = 1; i < 10; i = i + 1) {
+            System.out.println( i );
+        }
+        //task2
+
+        }
+        }
+
+
