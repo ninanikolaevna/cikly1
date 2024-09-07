f162646fa46d591b2b092c2b6a15166f7446f686
ninanikolaevna <ninanikolaevna.t@gmail.com>
2024-09-07 20:54:29 +0300

    10 tasks

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,93 @@
 public class Main {
     public static void main(String[] args) {
         //task1
+        {
+            System.out.println("Задача1");
+        }
         for (int i = 1; i < 10; i = i + 1) {
-            System.out.println( i );
+            System.out.println(i);
         }
         //task2
+        {
+            System.out.println("Задача2");
+        }
+        for (int i = 10; i > 1; i = i - 1) {
+            System.out.println(i);
+        }
+        //task3
+        {
+            System.out.println("Задача3");
+        }
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+//task4
+        {
+            System.out.println("Задача4");
+        }
+        for (int i = 10; i > -10; i = i - 1) {
+            System.out.println(i);
+        }
+//task5
+        {
+            System.out.println("Задача5");
+        }
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i);
+        }
+//task6
+        {
+            System.out.println("Задача6");
+        }
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+//task7
+        {
+            System.out.println("Задача7");
+        }
+        for (int i = 2; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+//task8
+        {
+            System.out.println("Задача8");
+        }
+//«Месяц …, сумма накоплений равна … рублей».
+        {
+            int money = 29000;
+        int total = 1;
+        for (int i = 1; i <= 12; i++) {
+            total = total + money;
+            System.out.println("Месяц " + i + " , сумма накоплений "+ total + " рублей");
+        }}
+        //task9
+        {
+            System.out.println("Задача9");
+            //«Месяц …, сумма накоплений равна … рублей».
+            int money = 29000;
+            int total = 0;
+            for (int i = 1; i <= 12; i++) {
+                total = total + money / 100;
+                total = total + money;
+                System.out.println("Месяц " + i + " Итого " + total);
+            }
+            //task10
+            System.out.println("Задача 10");
+            {
+                for (int i = 2; i <= 20; i += 2) {
+                    System.out.println( " 2 * " + i/2 +" = " + i );
+                }
 
+            }
         }
-        }
+
+    }
+}
+
+
+
+
+
 
 
